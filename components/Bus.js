55f4bb2f0280658b9c86ec0694be60b1d6f02ec6
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-03-18 12:09:10 +0800

    Now shows the LOAD and TYPE of bus

--- components/Bus.js
+++ components/Bus.js
@@ -7,10 +7,22 @@ function Bus(props){
             <h1>{bus.no}</h1>
 
             <p>{Math.round(bus.next.duration_ms / 60000) <= 0 ? 'Arriving' : Math.round(bus.next.duration_ms / 60000) + ' mins'}</p>
+            <ul>
+                <li>Type: {bus.next.type}</li>
+                <li>Load: {bus.next.load}</li>
+            </ul>
 
             <p>{Math.round(bus.next2.duration_ms / 60000) <= 0 ? 'Arriving' : Math.round(bus.next2.duration_ms / 60000) + ' mins'}</p>
+            <ul>
+                <li>Type: {bus.next2.type}</li>
+                <li>Load: {bus.next2.load}</li>
+            </ul>
 
             <p>{Math.round(bus.next3.duration_ms / 60000) <= 0 ? 'Arriving' : Math.round(bus.next3.duration_ms / 60000) + ' mins'}</p>
+            <ul>
+                <li>Type: {bus.next3.type}</li>
+                <li>Load: {bus.next3.load}</li>
+            </ul>
 
         </div>
     )
