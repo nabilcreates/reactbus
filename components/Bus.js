457c60385bd7e6b0789a08907b69b4c04e0dfa1a
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-04-13 04:08:38 +0800

    Shows the arrival time and also the type in one line

--- components/Bus.js
+++ components/Bus.js
@@ -1,41 +1,38 @@
-function Bus(props){
+class Bus extends React.Component{
 
-    let bus = props.bus_info
+    constructor(){
+        super()
+    }
 
-    return(
-        <div id='bus'>
-                <div id='container'>
-                    {/* Set the id to the load so that you can use it in css */}
-                <h1>{bus.no}</h1>
+    returnBusLoad(bus_load){
+        if(bus_load =='DD'){
+            return 'Single'
+        } else if(bus_load == 'SD'){
+            return 'Double'
+        }else{
+            return 'Unkown'
+        }
+    }
 
-                {/* NEXT */}
-                <p  id={bus.next.load ? bus.next.load.toLowerCase() : 'noload'}  >{Math.round(bus.next.duration_ms / 60000) >= 1 ? `${Math.round(bus.next.duration_ms / 60000)} Minutes` : 'Arriving'}</p>
-                <ul>
-                    {/* Double Deck or Single Deck */}
-                    {bus.next.type == 'DD' ? <li>Bus Type: Double Deck</li> : null}
-                    {bus.next.type == 'SD' ? <li>Bus Type: Single Deck</li> : null}
-                    {bus.next.type == '' ? <li>Bus Type: Single Deck</li> : null}
-                </ul>
+    render(){
+        let bus = this.props.bus_info
 
-                {/* NEXT2 */}
-                <p  id={bus.next2.load ? bus.next2.load.toLowerCase() : 'noload'}  >{Math.round(bus.next2.duration_ms / 60000) >= 1 ? `${Math.round(bus.next2.duration_ms / 60000)} Minutes` : 'Arriving'}</p>
-                <ul>
-                    {/* Double Deck or Single Deck */}
-                    {bus.next2.type == 'DD' ? <li>Bus Type: Double Deck</li> : null}
-                    {bus.next2.type == 'SD' ? <li>Bus Type: Single Deck</li> : null}
-                    {bus.next2.type == '' ? <li>Bus Type: Single Deck</li> : null}
-                </ul>
+        return(
+            <div id='bus'>
+                    <div id='container'>
+                        {/* Set the id to the load so that you can use it in css */}
+                    <h1>{bus.no}</h1>
 
-                {/* NEXT3 */}
-                <p  id={bus.next3.load ? bus.next3.load.toLowerCase() : 'noload'}  >{Math.round(bus.next3.duration_ms / 60000) >= 1 ? `${Math.round(bus.next3.duration_ms / 60000)} Minutes` : 'Arriving'}</p>
-                <ul>
-                    {/* Double Deck or Single Deck */}
-                    {bus.next3.type == 'DD' ? <li>Bus Type: Double Deck</li> : null}
-                    {bus.next3.type == 'SD' ? <li>Bus Type: Single Deck</li> : null}
-                    {bus.next3.type == '' ? <li>Bus Type: Unknown</li> : null}
-                </ul>
+                    {/* NEXT */}
+                    <p  id={bus.next.load ? bus.next.load.toLowerCase() : 'noload'}  >{Math.round(bus.next.duration_ms / 60000) >= 1 ? `${Math.round(bus.next.duration_ms / 60000)} Minutes [${this.returnBusLoad(bus.next.type)}]` : `Arriving [${this.returnBusLoad(bus.next.type)}]`}</p>
 
-                </div>
-        </div>
-    )
+                    <p  id={bus.next2.load ? bus.next2.load.toLowerCase() : 'noload'}  >{Math.round(bus.next2.duration_ms / 60000) >= 1 ? `${Math.round(bus.next2.duration_ms / 60000)} Minutes [${this.returnBusLoad(bus.next2.type)}]` : `Arriving [${this.returnBusLoad(bus.next2.type)}]`}</p>
+
+                    <p  id={bus.next3.load ? bus.next3.load.toLowerCase() : 'noload'}  >{Math.round(bus.next3.duration_ms / 60000) >= 1 ? `${Math.round(bus.next3.duration_ms / 60000)} Minutes [${this.returnBusLoad(bus.next3.type)}]` : `Arriving [${this.returnBusLoad(bus.next3.type)}]`}</p>
+
+
+                    </div>
+            </div>
+        )
+    }
 }
